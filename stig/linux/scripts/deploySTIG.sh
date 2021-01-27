a55336e4c907a981198f8e1f6839bb725a094f4f
Shruti Priya <shpriya@microsoft.com>
2021-01-27 09:19:10 -0800

    Testing script

--- stig/linux/scripts/deploySTIG.sh
+++ stig/linux/scripts/deploySTIG.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Installing scap-security-guide..."
 yum -y install scap-security-guide
 
@@ -14,4 +16,4 @@ echo "Running script-stig-updated.sh to deploy STIGs..."
 bash script-stig-updated.sh
 echo "Deployment completed successfully."
 echo "Running oscap scanner to evaluate if the stig is applied to the VM."
-oscap xccdf eval --profile stig-rhel7-disa --results results.xml --report coverage_report.htm /usr/share/xml/scap/ssg/content/ssg-rhel7-ds.xml 2>/dev/null
+oscap xccdf eval --profile stig-rhel7-disa --results results.xml --report coverage_report.htm /usr/share/xml/scap/ssg/content/ssg-rhel7-ds.xml 2>&1
